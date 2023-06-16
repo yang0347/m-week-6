c06239381f9a0e4c50c69c43459a1a337b84fd27
yuwei yang <yang0347@algonquinlive.com>
2023-06-16 13:05:08 -0400

    s

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log("js")
+console.log("JS fil add")
+
+const bodyTag = document.querySelector("body")
+
+bodyTag.innerHTML += `
+<section id="groceries">
+   <h2>Groceries</h2>
+   <ul id="groceryList"></ul>
+</section>
+`
+
+let groceryItems = ["apples", "bananas", "milk", "eggs", "bread", "dog food"]
+const groceryUl = document.getElementById("groceryList")
+
+// want to items one by one --- add "item of" (for what coming from where)
+for(item of groceryItems){
+    groceryUl.insertAdjacentHTML("beforeend", `<li>${item}</Li>`)
+}
+
